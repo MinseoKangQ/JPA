ec6fb177168c84c820e25fdec85c7609ad15e61d
MinseoKangQ <kms02171@naver.com>
2023-07-27 13:42:32 +0900

    단방향 연관관계

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,27 +1,42 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
-        EntityTransaction tx = em.getTransaction(); // 트랜잭션 받기
+        EntityTransaction tx = em.getTransaction();
         tx.begin(); // 트랜잭션 시작
 
-        // code
+
         try {
+
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
 
             tx.commit(); // 커밋
         } catch (Exception e) {
             tx.rollback();
         } finally {
-            em.close(); // 꼭 닫아야 함
+            em.close();
         }
         emf.close();
 
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,114 +1,42 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 public class Member {
-//
-//    @Id
-//    private Long id;
-//
-//    @Column(name = "name")
-//    private String username;
-//
-//    private Integer age;
-//
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createdDate;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//
-//    @Lob
-//    private String description;
-//
-//    public Member() {
-//    }
-//
-//    public Member(Long id, String username, Integer age, RoleType roleType, Date createdDate, Date lastModifiedDate, String description) {
-//        this.id = id;
-//        this.username = username;
-//        this.age = age;
-//        this.roleType = roleType;
-//        this.createdDate = createdDate;
-//        this.lastModifiedDate = lastModifiedDate;
-//        this.description = description;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
-//
-//    public RoleType getRoleType() {
-//        return roleType;
-//    }
-//
-//    public void setRoleType(RoleType roleType) {
-//        this.roleType = roleType;
-//    }
-//
-//    public Date getCreatedDate() {
-//        return createdDate;
-//    }
-//
-//    public void setCreatedDate(Date createdDate) {
-//        this.createdDate = createdDate;
-//    }
-//
-//    public Date getLastModifiedDate() {
-//        return lastModifiedDate;
-//    }
-//
-//    public void setLastModifiedDate(Date lastModifiedDate) {
-//        this.lastModifiedDate = lastModifiedDate;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+
 
     public Member() {
     }
 
-    public Member(Long id, String username) {
-        this.id = id;
-        this.username = username;
-    }
+//    public Member(Long id, String username, Long teamId) {
+//        this.id = id;
+//        this.username = username;
+//        this.teamId = teamId;
+//    }
 
     public Long getId() {
         return id;
@@ -125,4 +53,12 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
+
+//    public Long getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
 }
