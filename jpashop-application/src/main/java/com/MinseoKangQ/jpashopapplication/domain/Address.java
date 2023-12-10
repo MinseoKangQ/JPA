c93cb41ec76b41d02b3012c7feb0bf24d0e88d9e
MinseoKangQ <kms02171@naver.com>
2023-12-10 18:17:01 +0900

    엔티티 클래스 개발2

--- jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/domain/Address.java
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/domain/Address.java
@@ -4,9 +4,18 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
 @Embeddable
-@Getter
+@Getter // 값 타입은 값 변경을 불가능하게 하기 위해 @Setter 는 제거
 public class Address { // 내장 타입
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { } // JPA 스펙 상 기본 생성자 protected 로 설정
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
--- jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/domain/item/Item.java
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/domain/item/Item.java
@@ -1,5 +1,6 @@
 package com.MinseoKangQ.jpashopapplication.domain.item;
 
+import com.MinseoKangQ.jpashopapplication.domain.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
@@ -7,6 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +28,8 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
 }
