58af0a2e7d4d0de772de757260c3bdf7d17aca5d
MinseoKangQ <kms02171@naver.com>
2023-12-11 21:38:40 +0900

    회원 리포지토리 개발

---
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package com.MinseoKangQ.jpashopapplication.repository;
+
+import com.MinseoKangQ.jpashopapplication.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // 타입, 기본키
+    }
+
+    public List<Member> findAll() { // SQL은 테이블 대상으로 쿼리하지만, JPQL은 엔티티 객체에 대해 쿼리함
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
