d2349c2b1708ef26b5afe628706d0bb80738b4b0
MinseoKangQ <kms02171@naver.com>
2023-12-21 13:59:44 +0900

    주문 리포지토리 개발

---
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package com.MinseoKangQ.jpashopapplication.repository;
+
+import com.MinseoKangQ.jpashopapplication.domain.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    // public List<Order> findAll(OrderSearch orderSearch) { }
+}
