202b55b1689714606d02f027f79e0c633c1afaed
MinseoKangQ <kms02171@naver.com>
2023-12-28 00:00:07 +0900

    회원 등록

---
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package com.MinseoKangQ.jpashopapplication.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+    @NotEmpty
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
