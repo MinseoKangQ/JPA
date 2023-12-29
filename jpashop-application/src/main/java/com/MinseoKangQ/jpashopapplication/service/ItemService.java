1096ce1db8a724fd5f41747620e395eb6cdf41c1
MinseoKangQ <kms02171@naver.com>
2023-12-29 15:12:11 +0900

    변경 감지와 병합

--- jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/service/ItemService.java
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.MinseoKangQ.jpashopapplication.service;
 
+import com.MinseoKangQ.jpashopapplication.domain.item.Book;
 import com.MinseoKangQ.jpashopapplication.domain.item.Item;
 import com.MinseoKangQ.jpashopapplication.repository.ItemRepository;
 import java.util.List;
@@ -17,6 +18,22 @@ public class ItemService {
     @Transactional // 클래스 자체가 readOnly 이므로 작성
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     * 변경 감지를 사용하자!! 병합은 필드를 선택할 수 없다!!
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // findItem 은 영속 상태임
+
+        // 실무에서는 아래와 같이 의미있는 메소드로 값을 변경하자. 이렇게 해야 변경 지점이 모두 엔티티로 간다.
+        // findItem.change(price, name, stockQuantity);
+
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
