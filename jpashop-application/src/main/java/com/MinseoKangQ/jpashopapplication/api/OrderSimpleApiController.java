2b518bdc0cdb4b4a68f84abf44f6471cd91d3c5b
MinseoKangQ <kms02171@naver.com>
2024-02-05 14:25:07 +0900

    간단한 주문 조회 V2: 엔티티를 DTO로 변환

--- jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/api/OrderSimpleApiController.java
+++ jpashop-application/src/main/java/com/MinseoKangQ/jpashopapplication/api/OrderSimpleApiController.java
@@ -1,14 +1,19 @@
 package com.MinseoKangQ.jpashopapplication.api;
 
+import com.MinseoKangQ.jpashopapplication.domain.Address;
 import com.MinseoKangQ.jpashopapplication.domain.Order;
+import com.MinseoKangQ.jpashopapplication.domain.OrderStatus;
 import com.MinseoKangQ.jpashopapplication.repository.OrderRepository;
 import com.MinseoKangQ.jpashopapplication.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -31,5 +36,32 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // LAZY 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o)) // order를 dto로 변환함
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
