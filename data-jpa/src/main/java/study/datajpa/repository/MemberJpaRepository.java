76a77ff5cebfc77bb5e1a3211a1585e5ce9794b6
MinseoKangQ <kms02171@naver.com>
2024-03-17 20:18:48 +0900

    :sparkles: 공통 인터페이스 기능

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,17 +5,46 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
     @PersistenceContext
     private EntityManager em;
 
+    // 저장
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
 
+    // 삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        // JPQL 사용
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // Optional 로 조회
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    // 개수
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    // 단건 조회
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
---
+++ data-jpa/src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,51 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    // 저자
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    // 삭제
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    // 전체 조회
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
+                .getResultList();
+    }
+
+    // Optional 로 조회
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    // 개수
+    public long count() {
+        return em.createQuery("select count(t) from Team t", Long.class)
+                .getSingleResult();
+    }
+
+    // 단건 조회
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+
+}
